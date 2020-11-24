851fb4f8177b1e06719b91470108463205e5f836
Daniel Majewski <d.majewski.3453@gmail.com>
2020-11-23 23:55:11 -0700

    update

---
+++ game.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(e) {
+
+});
+
+var game;
+
+var gameDiv = document.getElementById('game');
+var gameHeaderDiv = document.getElementById('game-header');
+var gameTableDiv = document.getElementById('game-table');
+
+var buildGameTable = function () {
+
+}
