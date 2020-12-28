8d5fc60e213e41593bded8957cb3746ff8648ff9
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-28 00:39:27 -0700

    game display and routing

--- front/ngStatsSite/src/app/components/gamePage/game-main/game-main.component.ts
+++ front/ngStatsSite/src/app/components/gamePage/game-main/game-main.component.ts
@@ -1,6 +1,7 @@
 import { GameService } from './../../../services/game.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Game } from 'src/app/models/game';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-game-main',
@@ -12,10 +13,38 @@ export class GameMainComponent implements OnInit {
   @Input() gameId: number;
   game: Game;
 
-  constructor(private gameService: GameService) { }
+  constructor(private gameService: GameService, private currentRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.currentRoute.params.subscribe(params => {
+      console.log('Loading game: '+params['id']);
+      try {
+        let id = Number.parseInt(this.currentRoute.snapshot.paramMap.get('id'));
+        if (id) {
+          this.gameId = id;
+        }
+      } catch {
+      }
+      this.load();
+    }
+    );
     this.load();
+  }
+  ngOnChanges(): void {
+    try {
+      let id = Number.parseInt(this.currentRoute.snapshot.paramMap.get('id'));
+      this.currentRoute
+      if (id) {
+        this.gameId = id;
+      }
+    } catch {
+    }
+    console.log(this.gameId)
+    this.load();
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+
   }
 
   load(): void {
